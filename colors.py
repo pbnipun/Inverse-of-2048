b44ba9ada584308f3d34cdac9d55eabdb7b8a0b6
pbnipun <pb.nipun@gmail.com>
2021-06-18 15:20:21 -0400

    First Commit

---
+++ colors.py
@@ -0,0 +1,50 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#c2b3a9"
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {
+    2: "#fad74d",
+    4: "#f0b922",
+    8: "#ffdb4a",
+    16: "#fce130",
+    32: "#ede291",
+    64: "#e64c2e",
+    128: "#ff775c",
+    256: "#f29446",
+    512: "#f5b682",
+    1024: "#f2e8cb",
+    2048: "#fcefe6"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#ffffff",
+    4: "#ffffff",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#695c57",
+    2048: "#695c57"
+}
+
+CELL_NUMBER_FONTS = {
+    2: ("Helvetica", 25, "bold"),
+    4: ("Helvetica", 25, "bold"),
+    8: ("Helvetica", 25, "bold"),
+    16: ("Helvetica", 20, "bold"),
+    32: ("Helvetica", 20, "bold"),
+    64: ("Helvetica", 20, "bold"),
+    128: ("Helvetica", 15, "bold"),
+    256: ("Helvetica", 15, "bold"),
+    512: ("Helvetica", 15, "bold"),
+    1024: ("Helvetica", 15, "bold"),
+    2048: ("Helvetica", 15, "bold")
+}
